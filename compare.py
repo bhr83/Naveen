3dfd08f3d46fc1645849859905b5fc64b8c7d7a4
ibrahim <ibrahim2021chicago@gmail.com>
2023-11-30 10:57:07 +0530

    compare.yml

---
+++ compare.py
@@ -0,0 +1,15 @@
+import difflib, sys
+
+srcfile = sys.argv[1]
+destfile = sys.argv[2] 
+with open(srcfile) as src_file:
+    src_file_text = src_file.readlines()
+ 
+with open(destfile) as dest_file:
+    dest_file_text = dest_file.readlines()
+ 
+# Find and print the diff:
+for line in difflib.unified_diff(
+        src_file_text, dest_file_text, fromfile=srcfile, 
+        tofile=destfile, lineterm=''):
+    print(line)
